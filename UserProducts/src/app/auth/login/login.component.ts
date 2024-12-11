928a75564062c3aeb7ddf45b13e735d6fa90801b
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-12-11 19:01:22 +0330

    add form of login

--- UserProducts/src/app/auth/login/login.component.ts
+++ UserProducts/src/app/auth/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if (this.loginForm.valid){
+      const {username, password} = this.loginForm.value;
+      console.log(username);
+      console.log(password);
+    }
+  }
 
 }
