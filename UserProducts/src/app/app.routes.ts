3f951099d7d33e1bb9fb8454e41c45d7a7b69733
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-12-15 18:58:48 +0330

    add guard for restricting normal users

--- UserProducts/src/app/app.routes.ts
+++ UserProducts/src/app/app.routes.ts
@@ -5,10 +5,11 @@ import { UsersListComponent } from './users-list/users-list.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { AdminGuard } from './auth/admin.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    {path: 'addUser', component: AddUserComponent},
+    {path: 'addUser', component: AddUserComponent, canActivate: [AdminGuard]},
     {path: 'usersList', component: UsersListComponent},
     {path: 'editUser/:id', component: EditUserComponent},
     {path: 'addProducts', component: AddProductComponent},
